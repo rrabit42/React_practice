d1b51e229a4f13e2f28a2cbd60057491142422a0
rrabit42 <gegiraffe@naver.com>
2019-05-02 12:26:21 +0900

    이벤트 state props와 render함수

--- src/App.js
+++ src/App.js
@@ -10,15 +10,27 @@ class App extends Component{
     constructor(props){
         super(props);
         this.state ={
-            subject:{title:'WEB', sub:'World Wid Web!'},
+            mode: 'welcome',
+            subject:{title:'WEB', sub:'World Wide Web!'},
+            welcome:{title:'Welcome', desc:'Hello,React!!!'},
             contents:[
-                {id:1, title:'HTML', desc:'HTML is HYperText...'},
+                {id:1, title:'HTML', desc:'HTML is HyperText...'},
                 {id:1, title:'CSS', desc:'CSS is for design'},
                 {id:1, title:'JavaScript', desc:'JavaScript is for interactive'}
             ]
         }
     }
   render(){
+        console.log('App render');
+        var _title, _desc = null;
+        if(this.state.mode === 'welcome'){
+            _title = this.state.welcome.title;
+            _desc = this.state.welcome.desc;
+        }
+        else if(this.state.mode === 'read'){
+            _title = this.state.contents[0].title;
+            _desc = this.state.contents[0].desc;
+        }
     return(
         <div className="App">
           <Subject
@@ -26,7 +38,7 @@ class App extends Component{
               sub={this.state.subject.sub}>
           </Subject>
           <TOC data={this.state.contents}></TOC>
-          <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+          <Content title={_title} desc={_desc}></Content>
         </div>
     )
   }
